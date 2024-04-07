4278eeda207b78a7fac72b5f7cecb4c19683124e
julien-heysam <141445966+julien-heysam@users.noreply.github.com>
2024-04-06 23:45:11 -0700

    remove sentence-transformers

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import find_packages, setup
 
+
+def read_requirements():
+    with open("requirements.txt", "r") as req:
+        return req.read().splitlines()
+
+
 setup(
     name="src",
     version="0.1.0",
@@ -7,6 +13,7 @@ setup(
     author="Julien Wuthrich",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=read_requirements(),
     zip_safe=False,
     license="MIT",
 )
