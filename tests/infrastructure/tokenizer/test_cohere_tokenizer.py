881152b24cc8af8603da809d79e8fb993de2e1bd
julien-heysam <141445966+julien-heysam@users.noreply.github.com>
2024-04-07 17:41:58 -0700

    skip cohere test tokenizer

--- tests/infrastructure/tokenizer/test_cohere_tokenizer.py
+++ tests/infrastructure/tokenizer/test_cohere_tokenizer.py
@@ -9,9 +9,11 @@ def tokenizer():
     return CohereTokenizer(ChatCohereCommandR())
 
 
+@pytest.mark.skip("require api key")
 def test_length_function(tokenizer):
     assert tokenizer.length_function("hi text") == 2
 
 
+@pytest.mark.skip("require api key")
 def test_encode(tokenizer):
     assert tokenizer.encode("hi text") == [5649, 4518]
